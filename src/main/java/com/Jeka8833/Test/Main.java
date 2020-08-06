09e92b47513d70ce57c79c6e0006e8d859332fb6
Jeka8833 <eadercach@gmail.com>
2020-08-06 10:18:11 +0300

    Update script

--- src/main/java/com/Jeka8833/Test/Main.java
+++ src/main/java/com/Jeka8833/Test/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(new Main().getHelloWorld());
-        System.out.println("Test");
+        System.out.println("Test Change");
     }
 
 }
