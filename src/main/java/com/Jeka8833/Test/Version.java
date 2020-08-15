edf1ef64d3398968a6874022c359aeb2eba1ddf2
Jeka8833 <eadercach@gmail.com>
2020-08-15 12:58:11 +0300

    Update

---
+++ src/main/java/com/Jeka8833/Test/Version.java
@@ -0,0 +1,11 @@
+package com.Jeka8833.Test;
+
+public class Version {
+
+    /**
+    *This is a fully auto generated method, please don't touch it.
+    */
+    public static String getVersion() {
+        return "2.0.3";
+    }
+}
